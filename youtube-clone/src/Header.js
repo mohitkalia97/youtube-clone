f0c88d839635482f5c984a3a02b361f0d1773695
mohitkalia97 <102523042+mohitkalia97@users.noreply.github.com>
2023-06-15 13:50:13 +0200

    styled the header with the elements, by adding containers

--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import "./Header.css";
+
 import MenuIcon from '@mui/icons-material/MenuSharp';
 import SearchIcon from '@mui/icons-material/Search';
 import VideoCallIcon from '@mui/icons-material/VideoCall';
@@ -6,25 +9,35 @@ import AppsIcon from '@mui/icons-material/Apps';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Avatar from '@mui/material/Avatar';
 
+
+
 export default function Header() {
   return (
     <div className='header'>
-        <h1>Header</h1>
-        <MenuIcon />
-        <img 
-             className='header-logo'
-             src='https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg' 
-             alt=''
-        />
-        <input type="text" />
-        <SearchIcon />
-        <VideoCallIcon />
-        <AppsIcon />
-        <NotificationsIcon />
-        <Avatar 
-            alt="Mohit Kalia"
-            src="https://avatars.githubusercontent.com/u/102523042?s=400&u=0db4cdb4509217c74738029540cef5d17e773e56&v=4"
-        />
+
+        <div className='header-left'>
+            <MenuIcon />
+            <img 
+                className='header-logo'
+                src='https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg' 
+                alt=''
+            />
+        </div>
+        
+        <div className='header-input'>
+            <input type="text" />
+            <SearchIcon />
+        </div>
+
+       <div className='header-icons'>
+            <VideoCallIcon />
+            <AppsIcon />
+            <NotificationsIcon />
+            <Avatar 
+                alt="Mohit Kalia"
+                src="https://avatars.githubusercontent.com/u/102523042?s=400&u=0db4cdb4509217c74738029540cef5d17e773e56&v=4"
+            />
+       </div>
 
     </div>
   )
