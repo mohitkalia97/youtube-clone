1acb8b71ee492a682f89d97aa3ea3f50858dd6c5
mohitkalia97 <kalia.mohit1997@gmail.com>
2023-10-13 16:57:16 +0200

    captured user input, handled form to send data to backend

--- youtube-clone/src/Register.js
+++ youtube-clone/src/Register.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Register.css";
 
 export default function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+            const response = await fetch('/api/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username, password })
+            });
+            
+            const data = await response.json();
+            
+            if (data.success) {
+                // Handle successful registration, e.g., redirect to login page
+            } else {
+                // Handle registration failure
+            }
+        } catch (error) {
+            console.error("Error during registration:", error);
+        }
+      };
+    
   return (
     <div className="register-wrapper">
       <h1>Please Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </label>
         <div>
           <button type="submit">Submit</button>
