b8a4f5cf286e3c344384b6c7303a48d01e8ec08c
mohitkalia97 <kalia.mohit1997@gmail.com>
2023-11-13 10:38:05 +0100

    fixed asp.users error

--- backend/youtube-clone-backend/youtube-clone-backend/Models/User.cs
+++ backend/youtube-clone-backend/youtube-clone-backend/Models/User.cs
@@ -1,6 +1,8 @@
-﻿namespace youtube_clone_backend.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace youtube_clone_backend.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public int Id { get; set; }
 
